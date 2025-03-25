39f745a1bc09efcdea5619f301c7b3e0dc88d919
Luiz Antonio Maria Guidini <111287820+LuizGuidini@users.noreply.github.com>
2025-03-25 18:39:23 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     
 </head>
 <body>
@@ -13,10 +13,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <p>Bem Vindo!</p>
+                    <p>Cuidadoso</p>
                 </div>
                 <div class="col-md-6 text-md-end">
-                    <p>&copy; 2024 Crediário Ceará. Todos os direitos reservados.</p>
+                    <p>&copy; 2025 L2M DeploySmart. Todos os direitos reservados.</p>
                 </div>
             </div>
         </div>
