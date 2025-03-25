e593bd457c7333be19243b5a2d202985d53662b5
Luiz Antonio Maria Guidini <111287820+LuizGuidini@users.noreply.github.com>
2025-03-25 18:33:50 -0300

    Update master.php

--- master.php
+++ master.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crediário Ceará</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>CuidaDoso</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
     
 <body>
