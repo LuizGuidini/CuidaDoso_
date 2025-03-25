61ee87f91aecd4ae62c2f46b866e6e166c708814
LuizGuidini <luizantoniomg@hotmail.com>
2025-03-25 12:55:34 -0300

    incluindo php

---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container my-4">
+        <h1 class="text-center">Menu</h1>
+        <hr>
+        <nav class="nav justify-content">
+            <a style="align-items: flex-start;">
+                <img src="imagens/logocrediario.png" alt="Home" width="60" height="60">
+            </a> 
+            <a class="nav-link custom-nav-link mx-2" href="index.php">Pagina Inicial</a>
+            <a class="nav-link custom-nav-link mx-2" href="page2.php">Vitrine</a>
+            <a class="nav-link custom-nav-link mx-2" href="page3.php">Escolha um Produto</a>
+            <a class="nav-link custom-nav-link mx-2" href="page1.php">Area Usuário</a>
+        </nav>
+        <hr>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
