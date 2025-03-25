782b40515684c480edfeab5a7418ae61810981cf
Luiz Antonio Maria Guidini <111287820+LuizGuidini@users.noreply.github.com>
2025-03-25 18:36:55 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Principal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
     <div class="container my-4">
@@ -13,12 +13,15 @@
         <hr>
         <nav class="nav justify-content">
             <a style="align-items: flex-start;">
-                <img src="imagens/logocrediario.png" alt="Home" width="60" height="60">
+                <img src="assets/logo1.png" alt="Home" width="60" height="60">
             </a> 
-            <a class="nav-link custom-nav-link mx-2" href="index.php">Pagina Inicial</a>
-            <a class="nav-link custom-nav-link mx-2" href="page2.php">Vitrine</a>
-            <a class="nav-link custom-nav-link mx-2" href="page3.php">Escolha um Produto</a>
-            <a class="nav-link custom-nav-link mx-2" href="page1.php">Area Usuário</a>
+            <a class="nav-link custom-nav-link mx-2" href="index.php">
+                <h1>Entrar</h1> 
+            </a>
+            <a class="nav-link custom-nav-link mx-2" href="page2.php">
+                <h1>Entrar</h1>
+            </a>
+           
         </nav>
         <hr>
     </div>
