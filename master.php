29859b0cc12a3946eab3320e43b80392b095541c
LuizGuidini <luizantoniomg@hotmail.com>
2025-03-26 12:24:26 -0300

    testando

--- master.php
+++ master.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CuidaDoso</title>
     <link rel="stylesheet" href="styles/styles.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
-    
 <body>
-
     <div><?php include('menu.php'); ?></div>
-    <div><?php include($page_content); ?></div>
-    <div><?php include('rodape.php'); ?></div>        
-
+    <div class="container">
+        <?php include($page_content); ?>
+    </div>
+    <div><?php include('rodape.php'); ?></div>
+    <script src="scripts.js"></script>
 </body>
 </html>
