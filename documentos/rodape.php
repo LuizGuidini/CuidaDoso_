e008c38113f51b8f467532afbf69815de215b21f
Luiz Antonio Maria Guidini <111287820+LuizGuidini@users.noreply.github.com>
2025-03-25 21:08:52 -0300

    pasta documentos

---
+++ documentos/rodape.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/styles.css">
+    
+</head>
+<body>
+    
+    <footer class="footer mt-auto py-3 bg-light">
+      <div class="container d-flex justify-content-between align-items-center">
+        <span class="text-muted">L2M DeploySmart</span>
+        <span class="text-muted">© 2025 CuidaDoso. Todos os direitos reservados.</span>
+        <button id="panic-button" class="btn btn-danger">
+          <img src="images/panic-icon.jpg" alt="Botão de Pânico" class="panic-icon">
+        </button>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
